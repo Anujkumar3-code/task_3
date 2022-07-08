5fab7d45bf8d2e7ab08fc0ac793d7e129b6399ac
Anuj Kumar3 <anuj_kumar3@epam.com>
2022-07-08 09:28:17 +0530

    uploading the project

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+function About(){
+    return (
+        <div className='about' >
+        <h1 >About</h1>
+        <p className='para'>
+        Dolor dolore do dolor laborum aute sit. Ullamco ipsum exercitation ex anim exercitation sint ad officia nisi eu. Eu consequat ipsum ex tempor pariatur officia in amet. Laborum in dolore velit anim ullamco minim aute do id. Enim elit consequat nostrud nostrud esse labore ea laborum reprehenderit.
+       
+        </p>
+        </div>
+    )
+}
+export default About;
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Home(){
+    return (
+        <div className='home'>
+        <h1 >Home</h1>
+        <p className='para'>
+        Dolor dolore do dolor laborum aute sit. Ullamco ipsum exercitation ex anim exercitation sint ad officia nisi eu. Eu consequat ipsum ex tempor pariatur officia in amet. Laborum in dolore velit anim ullamco minim aute do id. Enim elit consequat nostrud nostrud esse labore ea laborum reprehenderit.
+       
+        </p> </div>
+    )
+}
+export default Home;
